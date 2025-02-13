ffcd36a4d1943ef7b692dd7491713698401600e6
Abdullah M <m.abdullah7077@outlook.com>
2025-02-13 12:12:21 +0000

    Test Change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, change has been made");
+        System.out.println("Hello, second change has been made");
     }
 }
