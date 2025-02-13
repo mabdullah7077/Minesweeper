76cdebfec48644a999da314b3dd537f223661609
Abdullah M <m.abdullah7077@outlook.com>
2025-02-13 13:47:04 +0000

    Added display tile method and comments

--- src/main/java/org/example/Tile.java
+++ src/main/java/org/example/Tile.java
@@ -20,15 +20,15 @@ public class Tile {
         isRevealed = true; // set is revealed boolean to true given previous if statement is not run
 
         if (isMine) {
-            System.out.println("Game over! You hit a mine!");
+            System.out.println("Game over! You blew up a mine!"); // print game over message if mine is hit
         } else if (adjacentMines == 0) {
-            System.out.println("Empty tile revealed.");
+            System.out.println("Empty tile revealed"); // print empty tile revealed if tile is not a mine and there are no adjacent mines
         } else {
-            System.out.println("Tile with " + adjacentMines + " adjacent mines revealed.");
+            System.out.println("Tile with " + adjacentMines + " adjacent mines revealed"); // print tile and adjacent mines revealed
         }
     }
 
-    // getters and setters
+    // getters for tile attributes
     public boolean isMine() {
         return isMine;
     }
@@ -42,8 +42,26 @@ public class Tile {
         return adjacentMines;
     }
 
+    // setter for adjacent mines attribute
     public void setAdjacentMines(int count) {
         this.adjacentMines = count;
+    }
+
+    public String displayTile(){
+        if (!isRevealed) { // if tile not yet revealed
+            return "-"; // display a dash on the tile
+            }
+
+        if (isMine) { // if tile is revealed and tile is a mine
+            return "M"; // display M for mine
+        }
+
+        if (adjacentMines > 0) { // if there any mines adjacent to the revealed tile
+            return String.valueOf(adjacentMines); // return number of adjacent mines
+        }
+
+        return " "; // if tile is revealed and no adjacent mines, return blank space
+
     }
 
 }
