b5b45655b75d4da673052c139391cae9c6102423
Abdullah M <m.abdullah7077@outlook.com>
2025-02-13 14:27:40 +0000

    added getter methods, setup starting board method and print board method

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -13,6 +13,39 @@ public class Board {
         this.height = boardHeight;
         this.numOfMines = boardMines;
         this.tiles = new Tile[width][height]; // create 2d array of tile objects with width and height dimensions
+        setupStartingBoard();
+        printBoard();
     }
+
+    public int getWidth(){ //getter method for board width
+        return width;
+    }
+
+    public int getHeight() { //getter method for board height
+        return height;
+    }
+
+    public Tile getTile(int x, int y){ // getter method for specific tile at given x and y coordinates
+        return tiles[x][y];
+    }
+
+    private void setupStartingBoard(){
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = new Tile(false);
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.print(tiles[x][y] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
 
 }
