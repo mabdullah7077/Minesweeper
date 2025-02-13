2623925a23cea19bbf34c9a50eec7caa3913fafc
Abdullah M <m.abdullah7077@outlook.com>
2025-02-13 13:58:58 +0000

    Added attributes and constructor to board class, and added comments

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -3,4 +3,16 @@ package org.example;
 import java.util.Random;
 
 public class Board {
+    private int width; //  width of board
+    private int height; // height of board
+    private int numOfMines; // number of mines
+    Tile[][] tiles; // create 2d array of tile objects
+
+    public Board (int boardWidth, int boardHeight, int boardMines) { // constructior to intialise board with dimensions and nuumber of mines
+        this.width = boardWidth;
+        this.height = boardHeight;
+        this.numOfMines = boardMines;
+        this.tiles = new Tile[width][height]; // create 2d array of tile objects with width and height dimensions
+    }
+
 }
