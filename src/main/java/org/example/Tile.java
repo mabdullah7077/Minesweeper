364c48dea838de4d8cb49b4bb0f64627f3a251c3
Abdullah M <m.abdullah7077@outlook.com>
2025-02-14 14:42:53 +0000

    Changed isRevealed boolean to isFlipped to avoid confusion with revealTileResult method. Added and amended comments

--- src/main/java/org/example/Tile.java
+++ src/main/java/org/example/Tile.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class Tile {
 
-    private boolean isRevealed; // determines if tile has been flipped
+    private boolean isFlipped; // determines if tile has been flipped
     private boolean isMine; // determines if tile is a mine
     private int adjacentMines; // stores number of mines adjacent to tile
 
     public Tile(boolean isMine) { // constructor for tile class taking isMine as a parameter
         this.isMine = isMine;
-        this.isRevealed = false;
+        this.isFlipped = false;
         this.adjacentMines = 0;
     }
 
@@ -17,8 +17,8 @@ public class Tile {
         return isMine;
     }
 
-    public boolean isRevealed() {
-        return isRevealed;
+    public boolean isFlipped() {
+        return isFlipped;
     }
 
 
@@ -31,40 +31,41 @@ public class Tile {
         this.adjacentMines = count;
     }
 
-    public void revealResult() { // outputs result once a tile has been flipped
-        if (isRevealed) { // if tile is already revealed, return nothing
+    public void revealTileResult() { // outputs result once a tile has been flipped
+        if (isFlipped) { // if tile is already flipped, return nothing
             return;
         }
 
-        isRevealed = true; // set is revealed boolean to true given previous if statement is not run
+        isFlipped = true; // set is revealed boolean to true given previous if statement is not run
 
         if (isMine) { // if user reveals a ine
             System.out.println("Game over! You blew up a mine!"); // print game over message if mine is hit
         } else if (adjacentMines == 0) { // if no adjacent mines in tile that user revealed
-            System.out.println("Empty tile revealed"); // print empty tile revealed if tile is not a mine and there are no adjacent mines
+            System.out.println("Empty tile revealed"); // print empty tile revealed to console if tile is not a mine and there are no adjacent mines
         } else {
-            System.out.println("Tile with " + adjacentMines + " adjacent mines revealed"); // print tile and adjacent mines revealed
+            System.out.println("Tile with " + adjacentMines + " adjacent mines revealed");
         }
+
     }
 
-    public String toString(){ // logic for board symbols
-        if (!isRevealed) { // if tile not yet revealed
+    public String toString(){ // logic for printing string value of board symbols
+        if (!isFlipped) { // if tile not yet flipped
             return "-"; // display a dash on the tile
             }
 
-        if (isMine) { // if tile is revealed and tile is a mine
+        if (isMine) { // if tile is flipped and tile is a mine
             return "X"; // display X for mine
         }
 
-        if (adjacentMines > 0) { // if there any mines adjacent to the revealed tile
+        if (adjacentMines > 0) { // if there any mines adjacent to the flipped tile
             return String.valueOf(adjacentMines); // return number of adjacent mines
         }
 
-        return " "; // if tile is revealed and no adjacent mines, return blank space
+        return " "; // if tile is flipped and no adjacent mines, return blank space
 
     }
 
-    public boolean isEmpty() {
+    public boolean isTileEmpty() { // method to check if tile is not a mine and there are no adjacent mines
         return !isMine && adjacentMines == 0;
     }
 
