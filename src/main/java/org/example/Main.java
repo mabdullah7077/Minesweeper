f5f95cb2bcf0698be7451253cc99469c80cee8f8
Abdullah M <m.abdullah7077@outlook.com>
2025-02-18 09:20:51 +0000

    implemented different difficulty levels and added comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game(10, 10, 10); // create game object and set paramenters
-        game.startGame(); // start the game
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Select difficulty: E/M/H"); // user inputs difficulty level
+            String input = scanner.next();
+            if (input.equalsIgnoreCase("E")){
+                Game game = new Game(5,5,2); // create easy game object
+                game.startGame();
+                break;
+            } else if (input.equalsIgnoreCase("M")) {
+                Game game = new Game(10, 10, 10); // create medium game object
+                game.startGame(); // start the game
+                break;
+            } else if (input.equalsIgnoreCase("H")){
+                Game game = new Game(15,15, 20); // create hard game object
+                game.startGame();
+                break;
+            } else {
+                System.out.println("Invalid input, please try again"); // Error message for invalid input
+            }
+        }
     }
 }
